783b71ffc4845dfcded6d08050dc02619e75d714
kuyawa <haxapp@gmail.com>
2017-07-19 10:24:45 -0400

    Initial Commit

---
+++ Webshot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Webshot
+//
+//  Created by Laptop on 7/19/17.
+//  Copyright © 2017 Armonia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
